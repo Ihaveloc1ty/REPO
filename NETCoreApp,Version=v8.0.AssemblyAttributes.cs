0c0cf41ccbc9d2be2b8f92d9519aeaf28c5dd3ac
Ihaveloc1ty <hanzelsar123@gmail.com>
2026-05-20 17:50:05 -0400

    Add files via upload

---
+++ NETCoreApp,Version=v8.0.AssemblyAttributes.cs
@@ -0,0 +1,53 @@
+{
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Projects\\Visual Studio\\SampleProjectS26\\",
+  "Documents": [
+    {
+      "AbsoluteMoniker": "D:0:0:{92749642-F760-4E17-B66C-A781C458F274}|SampleProjectS26\\SampleProjectS26.csproj|c:\\projects\\visual studio\\sampleprojects26\\sampleprojects26\\gameobject.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{92749642-F760-4E17-B66C-A781C458F274}|SampleProjectS26\\SampleProjectS26.csproj|solutionrelative:sampleprojects26\\gameobject.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{92749642-F760-4E17-B66C-A781C458F274}|SampleProjectS26\\SampleProjectS26.csproj|c:\\projects\\visual studio\\sampleprojects26\\sampleprojects26\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{92749642-F760-4E17-B66C-A781C458F274}|SampleProjectS26\\SampleProjectS26.csproj|solutionrelative:sampleprojects26\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    }
+  ],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": 0,
+          "Children": [
+            {
+              "$type": "Document",
+              "DocumentIndex": 0,
+              "Title": "GameObject.cs",
+              "DocumentMoniker": "C:\\Projects\\Visual Studio\\SampleProjectS26\\SampleProjectS26\\GameObject.cs",
+              "RelativeDocumentMoniker": "SampleProjectS26\\GameObject.cs",
+              "ToolTip": "C:\\Projects\\Visual Studio\\SampleProjectS26\\SampleProjectS26\\GameObject.cs",
+              "RelativeToolTip": "SampleProjectS26\\GameObject.cs",
+              "ViewState": "AQIAABEAAAAAAAAAAAAwwBMAAAAeAAAA",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-05-20T16:42:18.557Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 1,
+              "Title": "Program.cs",
+              "DocumentMoniker": "C:\\Projects\\Visual Studio\\SampleProjectS26\\SampleProjectS26\\Program.cs",
+              "RelativeDocumentMoniker": "SampleProjectS26\\Program.cs",
+              "ToolTip": "C:\\Projects\\Visual Studio\\SampleProjectS26\\SampleProjectS26\\Program.cs",
+              "RelativeToolTip": "SampleProjectS26\\Program.cs",
+              "ViewState": "AQIAAAAAAAAAAAAAAAAAABwAAAAAAAAA",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-05-20T16:41:47.068Z"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
